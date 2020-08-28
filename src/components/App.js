2440b34a470c0ffbf978792c403595dd1b6ddeff
dejongyeong <dejong.re2@gmail.com>
2020-08-28 22:47:35 +0100

    Initial Commit Hero Page #2
    Create Hero Section

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Hero from './Hero';
 
 function App() {
   return (
     <div className="App">
       <h1>Clean Canvas</h1>
+      <Hero />
     </div>
   );
 }
---
+++ src/components/Hero.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Hero() {
+  return (
+    <div>
+      <h1>Hero Page</h1>
+    </div>
+  );
+}
+
+export default Hero;
