d0966acde839f8505a84c608745a1a58d74b5ac2
dejongyeong <dejong.re2@gmail.com>
2020-09-13 22:10:07 +0100

    transition on heading #4

--- src/components/About.js
+++ src/components/About.js
@@ -19,13 +19,24 @@ const useStyles = makeStyles((theme) => ({
     height: '90%',
     textAlign: 'center',
     margin: '3% auto 0 auto',
+    '& h3': {
+      paddingTop: '3%',
+      paddingBottom: '2%',
+      color: '#2e585d',
+      fontWeight: '600',
+      letterSpacing: '3px',
+      animation: `$aboutEffect 2000ms ${theme.transitions.easing.easeInOut}`,
+    },
   },
   describe: {
-    color: '#393e46',
     margin: '2% auto',
-    lineHeight: '1.9rem',
-    fontSize: '1.1rem',
-    fontFamily: 'Lato, sans-serif',
+    '& p': {
+      lineHeight: '1.9rem',
+      fontSize: '1.02rem',
+      fontFamily: 'Lato, sans-serif',
+      color: '#393e46',
+      fontWeight: '400',
+    },
   },
   skills: {
     alignItems: 'center',
@@ -57,32 +68,37 @@ const useStyles = makeStyles((theme) => ({
     boxShadow: '1px 1px 3px rgb(182 211 213)',
     borderRadius: '0',
   },
+  '@keyframes aboutEffect': {
+    '0%': { transform: 'translate3d(0, -50px, 0)' },
+    '100%': { transform: 'translate3d(0, 0, 0)' },
+  },
 }));
 
 function About() {
   const classes = useStyles();
 
+  const description = (
+    <Typography variant="body1">
+      Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
+      doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo
+      inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+      Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut
+      fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem
+      sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit
+      amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora
+      incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad
+      minima veniam, quis nostrum exercitationem ullam corporis suscipit
+      laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum
+      iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae
+      consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
+    </Typography>
+  );
+
   return (
     <div className={classes.wrapper}>
       <div className={classes.container}>
-        <Typography variant="h3" style={{ paddingTop: '1%' }}>
-          About Me
-        </Typography>
-        <div className={classes.describe}>
-          "Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
-          ab illo inventore veritatis et quasi architecto beatae vitae dicta
-          sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-          aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
-          qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui
-          dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed
-          quia non numquam eius modi tempora incidunt ut labore et dolore magnam
-          aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum
-          exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex
-          ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in
-          ea voluptate velit esse quam nihil molestiae consequatur, vel illum
-          qui dolorem eum fugiat quo voluptas nulla pariatur?"
-        </div>
+        <Typography variant="h3">-- About Me --</Typography>
+        <div className={classes.describe}>{description}</div>
         <div className={classes.skills}>
           <div className={classes.paperWrap}>
             <Grid container className={classes.root} spacing={3}>
