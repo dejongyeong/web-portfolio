bb54629ab272583eb9ae9c5f58d904d981841861
dejongyeong <dejong.re2@gmail.com>
2022-01-14 00:53:32 +0000

    update project section in portfolio

--- src/data/SideProjects.js
+++ src/data/SideProjects.js
@@ -4,10 +4,12 @@ import PortfolioScreenshot from '../img/portfolio-v1.PNG';
 import SentimentAnalysisScreenshot from '../img/sentiment-analysis.PNG';
 import NoImagePreview from '../img/noimage.jpg'; // reference: https://luxmage.com/solution/the-retail-window-61.html
 import Covid19Tracker from '../img/covid-19-tracker.PNG';
+import PokemonWiki from '../img/pokemon-wiki.PNG';
 
 const projectTypes = {
   RECREATION: 'Recreation',
   COLLEGE: 'College',
+  CHALLENGE: 'Challenge',
 };
 
 const previewType = {
@@ -15,14 +17,33 @@ const previewType = {
   PRIVATE: 'Private',
 };
 
+const pokemonWiki = {
+  title: 'Pokemon Wiki',
+  description:
+    'A bring home coding challenge for a job interview that displays list of Pokémon and details of a specific Pokémon.',
+  date: '2022',
+  tech: ['NextJS', 'Styled-Components', 'React Query', 'React Apex Charts'],
+  github: 'https://github.com/dejongyeong/nextjs-challenge',
+  preview: 'https://nextjs-pokewiki.vercel.app/',
+};
+
 const covid19Tracker = {
   title: 'Covid-19 Tracker Application',
-  description: 'Visualizes the global and country specific total confirmed, recovered, and deceased cases retrieved from Open Disease Data API.',
+  description:
+    'Visualizes the global and country specific total confirmed, recovered, and deceased cases retrieved from Open Disease Data API.',
   date: '2021',
-  tech: ['React.JS', 'Chart.JS', 'React Simple Maps', 'CircleCI', 'Heroku', 'Material UI', 'Styled Components'],
+  tech: [
+    'React.JS',
+    'Chart.JS',
+    'React Simple Maps',
+    'CircleCI',
+    'Heroku',
+    'Material UI',
+    'Styled Components',
+  ],
   github: 'https://github.com/dejongyeong/covid-19-tracker',
-  preview: 'https://covid-19-tracker-apps.herokuapp.com/'
-}
+  preview: 'https://covid-19-tracker-apps.herokuapp.com/',
+};
 
 const portfolio = {
   title: 'Online Portfolio V1',
@@ -63,6 +84,17 @@ const mongoDbProject = {
 };
 
 export const sideProjects = [
+  {
+    key: uuid(),
+    type: projectTypes.CHALLENGE,
+    image: PokemonWiki,
+    date: pokemonWiki.date,
+    title: pokemonWiki.title,
+    description: pokemonWiki.description,
+    tech: pokemonWiki.tech,
+    github: pokemonWiki.github,
+    preview: pokemonWiki.preview,
+  },
   {
     key: uuid(),
     type: projectTypes.RECREATION,
